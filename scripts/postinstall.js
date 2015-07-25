a9ea1b8f406b7d379cf95b2ca665b76bd5df63d2
lorenzofox3 <laurent34azerty@gmail.com>
2015-07-25 11:47:43 +0200

    remove postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env node
-
-var fs = require('fs');
-var path = require('path');
-
-var localFile = path.normalize(__dirname + '/../config/elasticsearch.js');
-var projectFile = path.normalize(process.cwd() + '/../../config/elasticsearch.js');
-
-if (!fs.existsSync(projectFile)) {
-  console.log("coppying " + localFile + " to " + projectFile);
-  fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
-}
+//#!/usr/bin/env node
+//
+//var fs = require('fs');
+//var path = require('path');
+//
+//var localFile = path.normalize(__dirname + '/../config/elasticsearch.js');
+//var projectFile = path.normalize(process.cwd() + '/../../config/elasticsearch.js');
+//
+//if (!fs.existsSync(projectFile)) {
+//  console.log("coppying " + localFile + " to " + projectFile);
+//  fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
+//}
